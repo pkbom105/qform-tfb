3599d940920003927bae983c183573482e2109f5
Pannavith1 <pannavith1@tfb.local>
2026-05-05 14:35:59 +0700

    version 1.1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ['10.1.255.1'],
+ experimental: {
+    // ย้ายจาก allowedDevOrigins มาอยู่ใน serverActions แทน
+    serverActions: {
+      allowedOrigins: ['10.1.255.1', 'localhost:3000'],
   },
 };
 
